32da773bbd4cf8a522e7f6b03aaf41e13e5c1a57
Arun Balachandran <balachandran.arun94@gmail.com>
2021-07-14 00:57:55 -0500

    GROC-6: Centered app title and changed app theme to green

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -5,27 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.green,
+          appBarTheme: AppBarTheme(centerTitle: true)),
       home: Scaffold(
-          appBar: AppBar(title: Text('Hello world app!')),
-          body: Center(child: Text('Helooooooooooo world!!!!!!!!!'))),
+          appBar: AppBar(title: Text('Grocery Inventory')),
+          body: Center(
+              child: Text('This will contain the list of grocery items...'))),
     );
   }
 }
